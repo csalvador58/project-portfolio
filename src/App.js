7c37126f0c734536a0b06619ffaa36a5bac074f6
Chris Salvador <csalvador58@gmail.com>
2023-01-29 17:01:12 -0800

    Added sections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import {
+  Avatar,
   createTheme,
-  CssBaseline,
+  // CssBaseline,
   Grid,
   Paper,
   ThemeProvider,
+  Typography,
 } from '@mui/material';
-import Header from './layout/Header';
+// import Header from './layout/Header';
 import NavBar from './layout/NavBar';
-import Hero from './UI/Hero';
+import ProfilePic from './images/Salvador.jpg';
+import ProjectCard from './components/ProjectCard';
+// import Hero from './UI/Hero';
 // import { dark } from '@mui/material/styles/createPalette';
 
 export default function App() {
@@ -28,15 +32,81 @@ export default function App() {
       {/* <CssBaseline /> */}
       <Grid container direction='column'>
         <Grid item>
-          <Header />
+          <NavBar />
+          {/* <Header /> */}
+        </Grid>
+        <Grid item>{/* <Hero /> */}</Grid>
+        <Grid item>
+          ABOUT
+          <Avatar
+            alt='Chris Salvador'
+            src={ProfilePic}
+            sx={{ width: 100, height: 100 }}
+          />
+          <Typography variant='h5' component='h2'>
+            Quisque commodo facilisis tellus. Integer sodales lorem sed nisl.
+            Morbi consectetuer mauris quis odio. Ut dolor lorem, viverra vitae,
+            viverra eu, euismod nec, enim. Lorem ipsum dolor sit amet,
+            consectetuer adipiscing elit.
+          </Typography>
         </Grid>
         <Grid item>
-          <Hero />
+          <Paper elevation={0} sx={{ background: 'black' }} square>
+            <Typography variant='h6' component='h6' color='white'>
+              PROJECTS
+            </Typography>
+            <Grid container spacing={1}>
+              <Grid item xs={10}>
+                <ProjectCard></ProjectCard>
+                <Typography variant='body2' component='div' color='white'>
+                  Project
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <ProjectCard></ProjectCard>
+                <Typography variant='body2' component='div' color='white'>
+                  Project
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <ProjectCard></ProjectCard>
+                <Typography variant='body2' component='div' color='white'>
+                  Project
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <ProjectCard></ProjectCard>
+                <Typography variant='body2' component='div' color='white'>
+                  Project
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
-        <Grid item>About</Grid>
-        <Grid item>Experience</Grid>
-        <Grid item>Projects</Grid>
-        <Grid item>Contact</Grid>
+        <Grid item>
+        <Typography variant='h6' component='h6'>
+              EXPERIENCE
+            </Typography>
+          <Typography variant='h6' component='body'>
+            Quisque commodo facilisis tellus. Integer sodales lorem sed nisl.
+            Morbi consectetuer mauris quis odio. Ut dolor lorem, viverra vitae,
+            viverra eu, euismod nec, enim. Lorem ipsum dolor sit amet,
+            consectetuer adipiscing elit.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Paper elevation={0} sx={{ background: 'black' }} square>
+            <Typography variant='h6' component='h6' color='white'>
+              CONTACT
+            </Typography>
+            <Typography variant='h6' component='body' color='white'>
+            Quisque commodo facilisis tellus. Integer sodales lorem sed nisl.
+            Morbi consectetuer mauris quis odio. Ut dolor lorem, viverra vitae,
+            viverra eu, euismod nec, enim. Lorem ipsum dolor sit amet,
+            consectetuer adipiscing elit.
+          </Typography>
+          </Paper>
+        </Grid>
       </Grid>
       {/* <NavBar /> */}
     </ThemeProvider>
